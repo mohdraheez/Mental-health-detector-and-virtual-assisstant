7462aa1fbf69be9cdb134cb4489611ed9fd3ee1e
Mohammed Raheez <42299547+mohdraheez07@users.noreply.github.com>
2025-03-05 22:08:51 +0530

    added files

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4695ef209943ba49ce6163bf4a303741e429755935774e6a3503a07891ebb72
-size 2647
+oid sha256:45291eb1cb7890f08f69c3fa239217d7ebbfe26c2b1bbedcc63a260e00d4a355
+size 4109
---
+++ detect.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1bb9e62635f60954ac56a39af7efbef640036e7a7be0ce17c60c9486d6298af4
+size 2614
